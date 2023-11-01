e4f02a6f6df90f862013a78d05efa5e44309b56d
Shubham Gocher <shubhamgocher29@gmail.com>
2023-11-01 10:44:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+
+function App() {
+  return (
+    <div className="bg-green-500">
+    <div className="flex items-center justify-center">
+      React
+    </div>
+    </div>
+  );
+}
+
+export default App;
